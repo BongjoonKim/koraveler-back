d7d86e3852e3916d49c112cf625b0b27b4850888
bongjoonkim <bongjoonkim96@gmail.com>
2024-04-26 01:51:52 +0900

    koraveler-back 첫 푸쉬

---
+++ koraveler/src/main/java/server/koraveler/KoravelerApplication.java
@@ -0,0 +1,13 @@
+package server.koraveler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoravelerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoravelerApplication.class, args);
+    }
+
+}
---
+++ koraveler/src/test/java/server/koraveler/KoravelerApplicationTests.java
@@ -0,0 +1,13 @@
+package server.koraveler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoravelerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
