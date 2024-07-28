58ad2f149f887d7e6934150da49f852094e29894
bongjoonkim <bongjoonkim96@gmail.com>
2024-07-29 01:21:15 +0900

    글 저장까지 완료

--- koraveler/src/main/java/server/koraveler/blog/service/BlogServiceImpl/BlogServiceImpl.java
+++ koraveler/src/main/java/server/koraveler/blog/service/BlogServiceImpl/BlogServiceImpl.java
@@ -1,10 +1,17 @@
 package server.koraveler.blog.service.BlogServiceImpl;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import server.koraveler.blog.dto.DocumentsInfo;
+import server.koraveler.blog.model.Documents;
 import server.koraveler.blog.repo.BlogsRepo;
 import server.koraveler.blog.service.BlogService;
+
+import java.time.LocalDateTime;
 
 @Service
 public class BlogServiceImpl implements BlogService {
@@ -13,6 +20,30 @@ public class BlogServiceImpl implements BlogService {
 
     @Override
     public DocumentsInfo.DocumentsDTO createDocument(DocumentsInfo.DocumentsDTO documentsDTO) {
+
+        Documents documents = new Documents();
+        BeanUtils.copyProperties(documentsDTO, documents);
+        LocalDateTime now = LocalDateTime.now();
+
+        documents.setCreated(now);
+        documents.setUpdated(now);
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() != null) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            System.out.println("userDetails = " + userDetails);
+            String username = userDetails.getUsername();
+            documents.setCreatedUser(username);
+            documents.setUpdatedUser(username);
+            Documents afterDocument = blogsRepo.save(documents);
+
+            DocumentsInfo.DocumentsDTO newDocDTO = new DocumentsInfo.DocumentsDTO();
+            BeanUtils.copyProperties(afterDocument, newDocDTO);
+
+            return newDocDTO;
+        }
+
+
         return null;
     }
 }
