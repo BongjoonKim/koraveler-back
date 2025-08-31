0274c820006a2d330a8d37e7e6048db09181667a
bongjoonkim <bongjoonkim96@gmail.com>
2025-08-31 10:19:32 +0900

    git action 테스트

---
+++ koraveler/src/main/java/server/koraveler/chat/config/WebSocketConfig.java
@@ -0,0 +1,28 @@
+// WebSocketConfig.java
+package server.koraveler.chat.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic", "/queue"); // 메시지 브로커 활성화
+        config.setApplicationDestinationPrefixes("/app"); // 클라이언트에서 메시지 보낼 때 prefix
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS(); // SockJS 지원
+    }
+}
